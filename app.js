929157e12f31c0fb1c1d716f7ffc55e410a048ca
Taras <tonyjoss1990@gmail.com>
2023-05-10 19:33:03 -0400

    start auth

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const chalk = require("chalk");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,6 +14,8 @@ const port = process.env.PORT || 3000;
 //     next();
 // });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan("tiny"));
 app.use(express.static(path.join(__dirname, "/public")));
 app.use("/css", express.static(path.join(__dirname, "/public")));
@@ -29,9 +32,11 @@ const nav = [ // header nav
   ];
 const bookRouter = require('./src/routes/bookRoutes')(nav); // import from file and pass nav array
 const adminRouter = require('./src/routes/adminRoutes')(nav);
+const authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use('/books', bookRouter);
 app.use('/admin', adminRouter);
+app.use('/auth', authRouter);
 
 app.get('/', (req, res) => {
   res.render(
